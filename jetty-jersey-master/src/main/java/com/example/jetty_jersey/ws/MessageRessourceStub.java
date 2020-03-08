dd0048fa1fbbaf1d9a0a71f92a1b818bd833f573
AntoineDuriez <61229738+AntoineDuriez@users.noreply.github.com>
2020-03-08 14:34:55 +0100

    Update MessageRessourceStub.java

--- jetty-jersey-master/src/main/java/com/example/jetty_jersey/ws/MessageRessourceStub.java
+++ jetty-jersey-master/src/main/java/com/example/jetty_jersey/ws/MessageRessourceStub.java
@@ -1,12 +1,25 @@
 package com.example.jetty_jersey.ws;
 
-import java.util.ArrayList;
+import java.util.ArrayList; 
 import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import com.example.datanucleus.dao.Message;
-//A COMPLETER + JAVADOC
+//JAVADOC
 public class MessageRessourceStub {
 	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/Message")
 	public List<Message> getMessage() {
+		Message m1 = new Message("Salut", "message 1");
+		Message m2 = new Message("Au revoir", "message 2");
+		List<Message> listMessage = new ArrayList<Message>();
+		listMessage.add(m1);
+		listMessage.add(m2);
+		return listMessage;
 	}
 }
