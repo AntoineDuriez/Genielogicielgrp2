55d90e3f60442f867313a8a9e5c5ec490db3b6ac
AntoineDuriez <61229738+AntoineDuriez@users.noreply.github.com>
2020-03-08 14:33:33 +0100

    Update MarkerRessourceStub.java

--- jetty-jersey-master/src/main/java/com/example/jetty_jersey/ws/MarkerRessourceStub.java
+++ jetty-jersey-master/src/main/java/com/example/jetty_jersey/ws/MarkerRessourceStub.java
@@ -1,6 +1,6 @@
 package com.example.jetty_jersey.ws;
 
-import java.util.ArrayList;
+import java.util.ArrayList; 
 import java.util.List;
 
 import javax.ws.rs.Consumes;
@@ -12,18 +12,40 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import com.example.datanucleus.dao.*;
-// A COMPLETER + JAVADOC
+//JAVADOC
 public class MarkerRessourceStub {
 	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/Marker")
 	public List<Marker> getMarker() {
+		List<Marker> listMarker = new ArrayList<Marker>();
+		Marker m1 = new Marker("Hotel Boony", "Boony a la plage", 90, 90, null, null);
+		Marker m2 = new Marker("Universit�", "la gr�ve", 10, 30, null, null);
+		listMarker.add(m1);
+		listMarker.add(m2);
+		return listMarker;
 	}
 
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/map/{markerList}")
 	public Marker addMarker() {
+			Marker boony = new Marker("Lieu de r�sidence de Boony", "Boony home", 2, 3, null, null);
+			return boony;
 	}
 
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/map/{markerList}")
 	public boolean modifyMarker(Marker m) {
+		return true;
 	}
 
+	@DELETE
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/map/{markerList}")
 	public boolean deleteMarker(Marker m) {
+		return true;
 	}
 }
