8d9610a1af89f524ffa460c9c9050e3f0cd59f09
AkumalexTheExalted <57628600+AkumalexTheExalted@users.noreply.github.com>
2020-03-08 12:45:08 +0100

    Update home.js
    research for POST DELETE and PUT implementation in js

--- jetty-jersey-master/src/main/webapp/home.js
+++ jetty-jersey-master/src/main/webapp/home.js
@@ -1,4 +1,4 @@
-
+//js function for GET : get data from the server
 function getServerData(url, success){
     $.ajax({
         dataType: "json",
@@ -6,6 +6,38 @@ function getServerData(url, success){
     }).done(success);
 }
 
+//Recherche d'implémentation pour les ws
+//js function for POST : modify data which are already known
+function postServerData(url, data, success){
+	$.ajax({
+		type: "POST",
+		dataType: "json",
+		url: url,
+		data: data
+	}).done(success);
+}
+
+//js function for PUT : add new data
+function putServerData(url, data, success){
+	$.ajax({
+		type: "PUT",
+		dataType: "json",
+		url: url,
+		data: data
+	}).done(success);
+}
+
+//js function for DELETE : delete data
+function deleteServerData(url, success){
+	$.ajax({
+		method: "DELETE",
+		dataType: "json",
+		url: url,
+		data: data
+	}).done(success);
+}
+
+//code de base, à tester une fois les ws fini
 function callDone(result){
 	var templateExample = _.template($('#templateExample').html());
 
