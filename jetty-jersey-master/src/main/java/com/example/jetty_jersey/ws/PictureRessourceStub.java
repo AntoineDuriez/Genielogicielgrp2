64c35bc9436e807c8edd33800dd35dfc523d81f5
AntoineDuriez <61229738+AntoineDuriez@users.noreply.github.com>
2020-03-08 14:35:38 +0100

    Update PictureRessourceStub.java

--- jetty-jersey-master/src/main/java/com/example/jetty_jersey/ws/PictureRessourceStub.java
+++ jetty-jersey-master/src/main/java/com/example/jetty_jersey/ws/PictureRessourceStub.java
@@ -1,12 +1,27 @@
 package com.example.jetty_jersey.ws;
 
-import java.util.ArrayList;
+import java.util.ArrayList; 
 import java.util.List;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 import com.example.datanucleus.dao.Picture;
-//A COMPLETER + JAVADOC
+//JAVADOC
 public class PictureRessourceStub {
 
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/Picture")
 	public List<Picture> getPicture() {
+		Picture p1 = new Picture("Photo de vacande de Boony");
+		Picture p2 = new Picture("Profil tinder de Rémy");
+		List<Picture> listPic = new ArrayList<Picture>();
+		listPic.add(p1);
+		listPic.add(p2);
+		return listPic;
 	}
 }
