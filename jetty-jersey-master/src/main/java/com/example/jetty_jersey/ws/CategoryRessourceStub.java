6cf4424233ec6bdc7671ebee22cb7b09c49ce8d3
AntoineDuriez <61229738+AntoineDuriez@users.noreply.github.com>
2020-03-08 14:23:11 +0100

    Update CategoryRessourceStub.java

--- jetty-jersey-master/src/main/java/com/example/jetty_jersey/ws/CategoryRessourceStub.java
+++ jetty-jersey-master/src/main/java/com/example/jetty_jersey/ws/CategoryRessourceStub.java
@@ -1,13 +1,27 @@
 package com.example.jetty_jersey.ws;
 
-import java.util.ArrayList;
+import java.util.ArrayList; 
 import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import com.example.datanucleus.dao.Category;
 //A COMPLETER + JAVADOC
 public class CategoryRessourceStub {
 	
-	public List<Category> getCategory() {
-	}
 
+@GET
+@Produces(MediaType.APPLICATION_JSON)
+@Path("/Category")
+	public List<Category> getCategory() {
+		Category cat1 = new Category("Musique pour chat", "pour les chats", null);
+		Category cat2 = new Category("Pop", "pop", null);
+		List<Category> listCat = new ArrayList<Category>();
+		listCat.add(cat1);
+		listCat.add(cat2);
+		return listCat;
+
+}
 }
