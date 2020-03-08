d800cccac4c6f94aed2af9001373edc934bb7d61
AntoineDuriez <61229738+AntoineDuriez@users.noreply.github.com>
2020-03-08 14:34:15 +0100

    Update CategoryRessourceStub.java

--- jetty-jersey-master/src/main/java/com/example/jetty_jersey/ws/CategoryRessourceStub.java
+++ jetty-jersey-master/src/main/java/com/example/jetty_jersey/ws/CategoryRessourceStub.java
@@ -8,7 +8,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.example.datanucleus.dao.Category;
-//A COMPLETER + JAVADOC
+//JAVADOC
 public class CategoryRessourceStub {
 	
 
