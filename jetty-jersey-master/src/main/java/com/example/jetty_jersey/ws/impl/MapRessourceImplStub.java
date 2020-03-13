1a456fe4e033c5e05ffe248cfdacb6207df87551
AkumalexTheExalted <57628600+AkumalexTheExalted@users.noreply.github.com>
2020-03-13 15:24:24 +0100

    Update MapRessourceImplStub.java
    JAVADOC

--- jetty-jersey-master/src/main/java/com/example/jetty_jersey/ws/impl/MapRessourceImplStub.java
+++ jetty-jersey-master/src/main/java/com/example/jetty_jersey/ws/impl/MapRessourceImplStub.java
@@ -14,54 +14,80 @@ import javax.ws.rs.core.MediaType;
 
 import com.example.datanucleus.dao.Map;
 import com.example.datanucleus.dao.stub.MapDaoStub;
-import com.example.datanucleus.dao.stub.UserDaoStub;
-
+/**
+ * Real implementation of MapRessource with Dao stub calls
+ * @author GLA group 2
+ */
 public class MapRessourceImplStub {
-		@GET
-		@Produces(MediaType.APPLICATION_JSON)
-		@Path("/getmap")
-		public List<Map> getMaps() {		
-			MapDaoStub mds = new MapDaoStub(); //appel à l'implémentation bouchon
-			return mds.getMaps();
-		}
-
-		@PUT
-		@Consumes(MediaType.APPLICATION_JSON)
-		@Path("/addmap")
-		public boolean addMap() {
-			MapDaoStub mds = new MapDaoStub(); //appel à l'implémentation bouchon
-			return mds.addMap();
-		}
-
-		@POST
-		@Consumes(MediaType.APPLICATION_JSON)
-		@Path("/modifymap")
-		public boolean modifyMap(Map m) {
-			MapDaoStub mds = new MapDaoStub(); //appel à l'implémentation bouchon
-			return mds.modifyMap(m);
-		}
-	 	
-		@DELETE
-		@Consumes(MediaType.APPLICATION_JSON)
-		@Path("/deletemap")
-		public boolean deleteMap(Map m) {
-			MapDaoStub mds = new MapDaoStub(); //appel à l'implémentation bouchon
-			return mds.deleteMap(m);
-		}
-
-		@POST
-		@Consumes(MediaType.APPLICATION_JSON)
-		@Path("/sharemap")
-		public boolean shareMap(Map m) {
-			MapDaoStub mds = new MapDaoStub(); //appel à l'implémentation bouchon
-			return mds.shareMap(m);
-		}
-
-		@POST
-		@Produces(MediaType.APPLICATION_JSON)
-		@Path("/getinfosmap")
-		public Map getMyMap(String s) {
-			MapDaoStub mds = new MapDaoStub(); //appel à l'implémentation bouchon
-			return mds.getMyMap(s);
-		}
+	/**
+	 * getMaps implementation with dao stub
+	 * @return return a list of Maps
+	 */
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/getmap")
+	public List<Map> getMaps() {		
+		MapDaoStub mds = new MapDaoStub(); //appel à l'implémentation bouchon
+		return mds.getMaps();
+	}
+	/**
+	 * addMap implementation with dao stub
+	 * @return return true if a new map is added to the map list, else return false
+	 */
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/addmap")
+	public boolean addMap() {
+		MapDaoStub mds = new MapDaoStub(); //appel à l'implémentation bouchon
+		return mds.addMap();
+	}
+	/**
+	 * modifyMap implementation with dao stub
+	 * @param m A map which will be modify
+	 * @return return true if the map has been modified, else return false
+	 */
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/modifymap")
+	public boolean modifyMap(Map m) {
+		MapDaoStub mds = new MapDaoStub(); //appel à l'implémentation bouchon
+		return mds.modifyMap(m);
+	}
+	
+	/**
+	 * deleteMap implementation with dao stub
+	 * @param m A map which will be delete
+	 * @return return true if the map has been deleted, else return false
+	 */
+	@DELETE
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/deletemap")
+	public boolean deleteMap(Map m) {
+		MapDaoStub mds = new MapDaoStub(); //appel à l'implémentation bouchon
+		return mds.deleteMap(m);
+	}
+	/**
+	 * shareMap implementation with dao stub
+	 * @param m A map which will be share with user's friend
+	 * @return return true if the map has been shared, else return false
+	 */
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/sharemap")
+	public boolean shareMap(Map m) {
+		MapDaoStub mds = new MapDaoStub(); //appel à l'implémentation bouchon
+		return mds.shareMap(m);
+	}
+	/**
+	 * getMyMap implementation with dao stub
+	 * @param s A string which is the name of a map 
+	 * @return return the map to get information about it
+	 */
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/getinfosmap")
+	public Map getMyMap(String s) {
+		MapDaoStub mds = new MapDaoStub(); //appel à l'implémentation bouchon
+		return mds.getMyMap(s);
+	}
 }
