42e6f80f63185cee703026e0f85fd43f73dac7c9
AkumalexTheExalted <57628600+AkumalexTheExalted@users.noreply.github.com>
2020-03-13 15:11:07 +0100

    Update CategoryRessourceImplStub.java
    JAVADOC

--- jetty-jersey-master/src/main/java/com/example/jetty_jersey/ws/impl/CategoryRessourceImplStub.java
+++ jetty-jersey-master/src/main/java/com/example/jetty_jersey/ws/impl/CategoryRessourceImplStub.java
@@ -1,6 +1,5 @@
 package com.example.jetty_jersey.ws.impl;
 
-//import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.GET;
@@ -10,15 +9,20 @@ import javax.ws.rs.core.MediaType;
 
 import com.example.datanucleus.dao.Category;
 import com.example.datanucleus.dao.stub.CategoryDaoStub;
-
-
+/**
+ * Real implementation of CategoryRessource with Dao stub calls
+ * @author GLA group 2
+ */
 public class CategoryRessourceImplStub {
+	/**
+	 * getCategory implementation with dao stub
+	 * @return return a list of Category
+	 */
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/getcategory")
 		public List<Category> getCategory() {
 		CategoryDaoStub cds = new CategoryDaoStub();
 		return cds.getCategory();
-		}
-	
+		}	
 }
