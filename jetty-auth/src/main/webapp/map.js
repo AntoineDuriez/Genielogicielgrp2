4eeb52c5fd7b7aaeafd96edaa75aba475dc20f75
Alexandra <alex@test.test>
2020-04-05 15:05:49 +0200

    last update search address

--- jetty-auth/src/main/webapp/map.js
+++ jetty-auth/src/main/webapp/map.js
@@ -4,7 +4,6 @@
 //Elle fabrique une map passé en argument
 function afficherMap(divName){
     var map = L.map(divName).fitWorld();
-
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
@@ -32,4 +31,6 @@ function afficherMap(divName){
     }
 
     map.on('locationerror', onLocationError);
+    //barre de recherche d'adresse
+    L.Control.geocoder().addTo(map);
 }
